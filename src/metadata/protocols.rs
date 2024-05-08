48b10243a2a6f2c9090df85e88aaebb7226e5283
Rodrigo Bronzelle <bronzelle@gmail.com>
2024-05-08 17:11:32 -0300

    ✨ feat(metadata): Add Protocol API calls

---
+++ src/metadata/protocols.rs
@@ -0,0 +1,45 @@
+use anyhow::{anyhow, Result};
+use reqwest::{header::AUTHORIZATION, Client};
+use serde::{Deserialize, Serialize};
+
+use crate::core::Enso;
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Protocol {
+    slug: String,
+    url: String,
+}
+
+impl Enso {
+    pub async fn get_protocols(&self) -> Result<Vec<Protocol>> {
+        let client = Client::new();
+        let url = format!("{}/protocols", self.get_api_url());
+        let auth = format!("Bearer {}", self.api_key);
+        let response = client.get(&url).header(AUTHORIZATION, auth).send().await;
+        response
+            .map_err(|_| anyhow!("Couldn't get tokens"))?
+            .json::<Vec<Protocol>>()
+            .await
+            .map_err(|_| anyhow!("Couldn't parse result"))
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::core::Version;
+
+    use super::*;
+
+    #[tokio::test]
+    async fn test_get_protocols() {
+        let enso = Enso::new(
+            "1e02632d-6feb-4a75-a157-documentation".to_string(),
+            Version::V1,
+        );
+
+        let projects = enso.get_protocols().await;
+
+        assert!(projects.is_ok());
+    }
+}
